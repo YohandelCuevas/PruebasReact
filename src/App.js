70f1de53128b930009e03c518e1ca7b7d45f2c3c
Nathalia Mendez <nmendezweb@gmail.com>
2019-04-03 11:47:13 -0400

    Prueba_Login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import{
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom';
 
+
+const fakeAuth = {
+  isAuthenticade: false,
+  authenticade(cb){
+    this.isAuthenticade = true
+    setTimeout(cb,100) //fake async
+  },
+  signout(cb){
+    this.isAuthenticade = false
+    setTimeout(cb,100)
+  }
+}
+
+const Public = () =><h3>Public</h3>
+const Protected = () =><h3>Protected</h3>
+
+//LOGIN CLASS
+
+class Login extends React.Component {
+
+  state={
+    reditectToReferrer: false
+  }
+
+  login = ()=> {
+    fakeAuth.authenticade(()=>{
+      this.setState(()=>({
+        reditectToReferrer: true
+      }))
+    })
+  }
+
+  render(){
+    const {reditectToReferrer} = this.state
+    const {from} = this.props.location.state || {from: {pathname: '/'}}
+
+    if (reditectToReferrer === true){
+      return(
+        <Redirect to={from}/>
+      )
+    }
+   return(
+     <div>
+       <p>You must log in to view this page at {from.pathname}</p>
+       <button onClick={this.login}>Log in</button>
+     </div>
+   )
+  }
+}
+
+//Private route
+
+const PrivateRoute = ({component: Component,...rest}) =>(
+  <Route {...rest} render = {(props)=>(
+    fakeAuth.isAuthenticade === true 
+    ? <Component {...props}/>
+    : <Redirect to={{
+      pathname: '/login',
+      state : {from: props.location}
+    }}/>
+  )}/>
+)
+
+
+const AuthButton = withRouter( ({history}) =>(
+  fakeAuth.isAuthenticade === true ? 
+  <p>Welcome! <button onClick={()=> {
+    fakeAuth.signout( ()=> history.push('/') )
+  }}>Sing out</button> </p>
+  : <p>You're not logged in.</p>
+));
+
+//App class
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Router>
+          <div>
+          <AuthButton/>
+          <ul>
+            <li><Link to='/public'>Public page</Link></li>
+            <li><Link to='/protected'>Protected page</Link></li>
+          </ul>
+
+          <Route path='/public' component ={Public}/>
+          <Route path='/login' component ={Login}/>
+          <PrivateRoute path='/protected' component ={Protected}/>
+          </div>
+        </Router>
+       
+      
     );
   }
 }
